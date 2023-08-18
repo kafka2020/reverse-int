962d2182cd686f0d71cc3b8a28798cbb9d8dc7f7
Andrew S <shinkarev89@gmail.com>
2023-08-18 22:35:42 +0300

    feat: Task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString()
+    let arr = Array.from(str)
+    arr.reverse()
+    str = arr.join('')    // Join array back into a string
+    return parseInt(str)     // Converts arr into a integer from a string
 }
